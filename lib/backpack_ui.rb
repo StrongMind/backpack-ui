9b8152616f3111869e00de4d51867e4beb52d375
Josh Brito <josh.brito@strongmind.com>
2024-07-05 09:47:03 -0700

    (S) Initial commit
    Co-authored-by: CJ Scirone <cj.scironeii@strongmind.com>

---
+++ lib/backpack_ui.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "backpack_ui/version"
+
+module BackpackUi
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/backpack_ui_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe BackpackUi do
+  it "has a version number" do
+    expect(BackpackUi::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end

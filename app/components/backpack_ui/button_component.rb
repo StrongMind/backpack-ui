34e69794dfecf4d43376a1da98e65cb93d0f7089
Josh Brito <josh.brito@strongmind.com>
2024-07-05 14:02:35 -0700

    (B) - Allow type to be passed to button component
    Co-authored-by: CJ Scirone <cj.scironeii@strongmind.com>

--- app/components/backpack_ui/button_component.rb
+++ app/components/backpack_ui/button_component.rb
@@ -2,9 +2,25 @@
 
 module BackpackUi
   class ButtonComponent < ViewComponent::Base
-    def initialize(name:, icon:)
+    attr_reader :type
+
+    def initialize(name:, icon:, type:)
       @name = name
       @icon = icon
+      @type = type
+    end
+
+    def style
+      {
+        primary: "sm-btn-primary",
+        secondary: "sm-btn-secondary",
+        tertiary: "sm-btn-tertiary",
+        outline: "sm-btn-outline",
+        outline_dark: "sm-btn-outline-dark",
+        link: "sm-btn-link",
+        delete: "sm-btn-delete",
+        no_outline: "sm-btn-no-outline",
+      }[type.to_sym] + " sm-btn"
     end
   end
 end
